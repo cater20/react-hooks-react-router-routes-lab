ef6d99d1d59abe83edf3c4620d73a554c06bcf92
chrisphine <onyangoketta@gmail.com>
2022-07-22 13:23:22 +0300

    updating

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,15 +2,10 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return (<div>
+  return (
     <h1>Actors page</h1>
-    <div><ul>
-      <li>Benedict Cumberbatch</li>
-      <li>Justin Timberlake</li>
-      <li>Anna Kendrick</li>
-      <li>Tom Cruise</li>
-      </ul></div>
-  </div>);
+    
+  );
 }
 
 export default Actors;
